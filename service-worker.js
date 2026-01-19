1f75e22b98500bf02fa9585dda73bcde3c2d00ef
mailquantaratech-cell <mailquantaratech@gmail.com>
2026-01-20 02:26:39 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,1 @@
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open("reading-cache").then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/manifest.json"
-      ]);
-    })
-  );
-});
+
